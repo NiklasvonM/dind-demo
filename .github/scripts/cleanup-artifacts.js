aaa27de6ce4c313d6b023f31acd02a3df9805170
NiklasvonM <niklas.von.moers2@t-online.de>
2024-09-20 22:51:37 +0200

    try to fix

--- .github/scripts/cleanup-artifacts.js
+++ .github/scripts/cleanup-artifacts.js
@@ -1,37 +1,39 @@
-module.exports = async ({github, context}) => {
-    const image = process.env.IMAGE_NAME;
-    console.log(`Cleaning up old tags for image: ${image}`);
-    const tags = await github.rest.packages.getAllPackageVersionsForPackageOwnedByUser({
+module.exports = async ({ github, context }) => {
+  const image = process.env.IMAGE_NAME;
+  console.log(`Cleaning up old tags for image: ${image}`);
+  const packageVersions = await github.rest.packages.getAllPackageVersionsForPackageOwnedByUser({
       package_type: 'container',
       username: context.repo.owner,
       package_name: image
-    });
-  
-    const now = new Date();
-    // We don't want to keep the images tagged with the commit hash at all.
-    const daysToKeep = 0;
-  
-    for (const tag of tags.data) {
-      // Is commit hash or starts with "sha"
-      const isCommitHashTag = (/^[0-9a-f]{40}$/.test(tag.name) || tag.name.startsWith('sha')) && tag.name != 'main';
-  
-      if (isCommitHashTag) {
-        const createdAt = new Date(tag.created_at);
-        const daysOld = (now - createdAt) / (1000 * 60 * 60 * 24);
-  
-        if (daysOld > daysToKeep) {
-          console.log(`Deleting tag: ${tag.name}`);
-          try {
-            await github.rest.packages.deletePackageVersionForAuthenticatedUser({
-              package_type: 'container',
-              package_name: image,
-              package_version_id: tag.id
-            });
-          } catch (error) {
-            console.log(`Failed to delete tag: ${tag.name}`);
-            console.log(error);
+  });
+
+  const now = new Date();
+  const daysToKeep = 0; // We don't want to keep images tagged with the commit hash
+
+  for (const packageVersion of packageVersions.data) {
+      const tags = packageVersion.metadata.container.tags;
+
+      for (const tag of tags) {
+          const isCommitHashTag = (/^[0-9a-f]{40}$/.test(tag) || tag.startsWith('sha')) && tag !== 'main';
+
+          if (isCommitHashTag) {
+              const createdAt = new Date(packageVersion.created_at);
+              const daysOld = (now - createdAt) / (1000 * 60 * 60 * 24);
+
+              if (daysOld > daysToKeep) {
+                  console.log(`Deleting tag: ${tag} (package version ID: ${packageVersion.id})`);
+                  try {
+                      await github.rest.packages.deletePackageVersionForAuthenticatedUser({
+                          package_type: 'container',
+                          package_name: image,
+                          package_version_id: packageVersion.id
+                      });
+                  } catch (error) {
+                      console.log(`Failed to delete tag: ${tag} (package version ID: ${packageVersion.id})`);
+                      console.log(error);
+                  }
+              }
           }
-        }
       }
-    }
-  };
+  }
+};
