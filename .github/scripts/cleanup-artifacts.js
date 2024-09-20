1a17c3cf46c9627fb3e30170a81f1fb769dddc3b
Niklas von Moers <49690836+NiklasvonM@users.noreply.github.com>
2024-09-20 22:54:17 +0200

    Add Cleanup (#3)
    * add cleanup

---
+++ .github/scripts/cleanup-artifacts.js
@@ -0,0 +1,34 @@
+module.exports = async ({ github, context }) => {
+  const image = process.env.IMAGE_NAME;
+  console.log(`Cleaning up old tags for image: ${image}`);
+  const packageVersions = await github.rest.packages.getAllPackageVersionsForPackageOwnedByUser({
+      package_type: 'container',
+      username: context.repo.owner,
+      package_name: image
+  });
+
+  const now = new Date();
+  const daysToKeep = 0; // We don't want to keep images tagged with the commit hash
+
+  for (const packageVersion of packageVersions.data) {
+      const tags = packageVersion.metadata.container.tags;
+
+      for (const tag of tags) {
+          const isCommitHashTag = (/^[0-9a-f]{40}$/.test(tag) || tag.startsWith('sha')) && tag !== 'main';
+
+          if (isCommitHashTag) {
+              const createdAt = new Date(packageVersion.created_at);
+              const daysOld = (now - createdAt) / (1000 * 60 * 60 * 24);
+
+              if (daysOld > daysToKeep) {
+                  console.log(`Deleting tag: ${tag} (package version ID: ${packageVersion.id})`);
+                  await github.rest.packages.deletePackageVersionForAuthenticatedUser({
+                      package_type: 'container',
+                      package_name: image,
+                      package_version_id: packageVersion.id
+                  });
+              }
+          }
+      }
+  }
+};
